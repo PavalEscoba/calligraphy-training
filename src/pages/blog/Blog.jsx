5d6144664e7b41fd6eed8576ab643ec73570891d
Pavel Metlitsky <p.metlitsky@itransition.com>
2022-01-14 16:30:24 +0300

    WIP: section 18 done

--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
+
+import reducers from './reducers';
+import PostList from './components/PostList'
 
 const Blog = () => {
-  return <p> Blog Posts</p>;
+  const store = createStore(reducers, applyMiddleware(thunk))
+  return (
+        <Provider store={store}>
+          <div className="blog">
+            <div className="row">
+              <div className="col-12">
+                <h1 className="section-title">
+                  Blog
+                </h1>
+                <p className="subtitle">List of blog posts</p>
+              </div>
+            </div>
+            <div className="col-12">
+              <PostList />
+            </div>
+          </div>
+        </Provider>
+  );
 };
 
 export default Blog;
